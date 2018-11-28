16e894cc3fd79a1f8480e33e02e33a4c9f16dea6
Jill Cates <cates.jill@gmail.com>
2018-11-28 17:00:46 -0500

    Move mappers to helper file

--- medcodes/drug_codes.py
+++ medcodes/drug_codes.py
@@ -1,21 +1,5 @@
-atc_level_one = {
-    'A': 'alimentary tract and metabolism',
-    'B': 'blood and blood forming organs',
-    'C': 'cardiovascular system',
-    'D': 'dermatologicals',
-    'G': 'genito-urinary system and sex hormones',
-    'H': 'systemic hormonal preparations, excluding sex hormones and insulins',
-    'J': 'anti-infectives for systemic use',
-    'L': 'antineoplastic and immunomodulating agents',
-    'M': 'musculo-skeletal system',
-    'N': 'nervous system',
-    'P': 'antiparasitic products, insecticides and repellents',
-    'Q': 'veterinary drug',
-    'R': 'respiratory system',
-    'S': 'sensory organs',
-    'V': 'various'
-}
-
+from _mappers import atc_level_one
+ 
 def atc_info(atc_id, level=1):
     """Gets information for a given ATC id."""
     info = None
